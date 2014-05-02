efe613835afbef175e62b2bf6d822947b6d5b08f
rachellogie <rachel.logie1@gmail.com>
2014-05-02 15:17:48 -0600

    Added validation tests for name presence and options presence

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,7 @@ class Restaurant < ActiveRecord::Base
   DIETARY_OPTIONS = ["vegan", "dairy free", "vegetarian option", "gluten free", "organic", "non-GMO"]
   acts_as_taggable_on :dietary_options
 
+  validates :name, presence: true
+  validates :dietary_option_list, presence: true
+
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper'
 describe Restaurant do
 
   it 'validates the presence of the name' do
-    pending
     restaurant = Restaurant.create!(name: "Shine", location:"Boulder", dietary_option_list:"gluten free")
-
     restaurant.name = ""
+    expect(restaurant).to_not be_valid
+  end
 
+  it 'validates the presence of the dietary options list' do
+    restaurant = Restaurant.create!(name: "Shine", location:"Boulder", dietary_option_list:"gluten free")
+    restaurant.dietary_option_list = nil
     expect(restaurant).to_not be_valid
   end
 
