f098eb8e075f25b8eed651f4dfea6daa895d5aa8
rachellogie <rachel.logie1@gmail.com>
2014-05-24 10:45:28 -0600

    Replaced you are not signed in in nav bar with sign up or sign in

--- spec/support/capybara_helpers.rb
+++ spec/support/capybara_helpers.rb
@@ -2,12 +2,11 @@ module CapybaraHelpers
 
   def sign_up_user
     visit '/'
-    click_on 'Sign in'
     click_on 'Sign up'
     fill_in 'Email', with: 'bob@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
-    click_on 'Sign up'
+    click_button 'Sign up'
   end
 
   def sign_in_user
